3ac70d7d97516a8ff3c32a74953240b871c919f2
PlamenCvetkov <cvetkov.plamen.1991@gmail.com>
2024-01-19 08:40:37 +0200

    Committing remaining changes

---
+++ script/Interactions.s.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+// Fund
+// Withdraw
+
+pragma solidity ^0.8.18;
+import {Script} from "forge-std/Script.sol";
+
+contract Interactions is Script {
+    function run() external {
+        
+    }
+}
+
+contract WithdrawFundMe is Script{
+
+}
